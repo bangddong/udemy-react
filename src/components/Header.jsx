c1c1f390b69dbc3d05473a4dad1c651a16a5d48b
bangddong <gusehdqkd@naver.com>
2024-01-17 11:16:03 +0900

    Refactor  - logo 이미지 import 방식으로 변경  - html 규격에 따라 div 태그를 header 태그로 변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,10 @@
+import logo from "../assets/investment-calculator-logo.png";
+
 export default function Header() {
   return (
-    <div id="header">
-      <img
-        src="investment-calculator-logo.png"
-        alt="investment-calculator-logo"
-      />
+    <header id="header">
+      <img src={logo} alt="investment-calculator-logo" />
       <h1>Investment Calculator</h1>
-    </div>
+    </header>
   );
 }
