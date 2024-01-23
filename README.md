c5e3cc41da4eae327c5baf2346c59ce9bb89d4d7
bangddong <gusehdqkd@naver.com>
2024-01-23 18:18:52 +0900

    add - README.MD  - 학습내용 정리노트 추가  - ref 기초 학습부분 추가

--- README.md
+++ README.md
@@ -1,3 +1,40 @@
 Udemy 강의 수강
 
 # Refs & Portals
+
+### useRef()
+
+렌더링에 사용하지 않는 값을 보관하는 내장 리액트 훅이며 사용법은 다음과 같다.
+
+```
+import { useRef } from "react";
+
+function Component() {
+  const refValue = useRef(0);
+  const inputFlag = useRef(false);
+}
+```
+
+useState와 같이 매개 변수로 해당 객체의 초기 값을 지정할 수 있으며  
+다음 렌더링이 일어나도 여전히 똑같은 객체를 반환한다.
+
+값을 저장하여 나중에 읽을 수 있다는 점이 state와 비슷할 수 있으나  
+ref는 값의 변화가 일어나도 **다시 렌더링 되지 않는다**
+
+따라서 값의 변화에 따른 렌더링이 일어나지 않으니 화면에 표시되는 값을  
+저장하기에는 적합하지 않으니 이런 상황에는 state를 사용해야 한다.
+
+일반 변수, state, ref를 비교해보면 다음과 같다.
+
+```
+function Component() {
+  // 다시 렌더링 될 때 값 또한 초기화 된다!
+  let normalVariable = 0;
+
+  // setStateVariable를 통해 상태가 바뀌면 렌더링이 발생한다!
+  const [stateVariable, setStateVariable] = useState(0);
+
+  // 다시 렌더링 되어도 값을 기억하며 참조가 변해도 렌더링은 발생하지 않는다!
+  const refVariable = useRef(0);
+}
+```
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,13 +1,33 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const playerName = useRef(0);
+  let test = 0;
+  console.log(test);
+  test = test + 1;
+  console.log(test);
+  const [enteredPlyaerName, setEnteredPlayerName] = useState(null);
+
+  function handleClick() {
+    // ref를 통해 input method에 접근할 수 있다.
+    setEnteredPlayerName(playerName.current.value);
+  }
+
   return (
     <section className="text-center">
-      <h2 className="text-main">Welcome unknown entity</h2>
+      <h2 className="text-main">
+        Welcome {enteredPlyaerName ?? "unknown entity"}
+      </h2>
       <p className="flex items-center justify-center">
         <input
+          ref={playerName}
           className="border-[1px] border-solid border-main bg-[#192f2b] rounded-[4px] rounded-tr-none rounded-br-none p-1 text-[#d1f0ec]"
           type="text"
         />
-        <button className="cursor-pointer bg-main border-[1px] border-solid border-main py-[0.4rem] px-4 text-[#061e1a] rounded-tr-[4px] rounded-br-[4px] hover:bg-hover hover:border-hover">
+        <button
+          onClick={handleClick}
+          className="cursor-pointer bg-main border-[1px] border-solid border-main py-[0.4rem] px-4 text-[#061e1a] rounded-tr-[4px] rounded-br-[4px] hover:bg-hover hover:border-hover"
+        >
           Set Name
         </button>
       </p>
