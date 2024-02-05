ee75503569e6636717f7e9a563aab8eabbf81c12
bangddong <gusehdqkd@naver.com>
2024-02-05 16:57:41 +0900

    feat - basic router  - router 기본 사용기능 실습

--- spa/src/App.js
+++ spa/src/App.js
@@ -1,5 +1,43 @@
+import {
+  RouterProvider,
+  createBrowserRouter,
+  // createRoutesFromElements,
+  // Route,
+} from "react-router-dom";
+
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
+import ProductDetailPage from "./pages/ProductDetail";
+
+// JSX 기반 라우터 사용법
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>
+// );
+
+// const router = createBrowserRouter(routeDefinitions);
+
+// 객체 기반 라우터 사용법
+// 모든 경로에서는 슬래시(/) 제거시 상대경로로 인식
+const router = createBrowserRouter([
+  {
+    path: "/root",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, path: "", element: <HomePage /> },
+      { path: "products", element: <ProductsPage /> },
+      { path: "products/:productId", element: <ProductDetailPage /> },
+    ],
+  },
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
