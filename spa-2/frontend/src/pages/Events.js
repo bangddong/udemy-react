d439c9e990ef24f6fbe9fdcaf0c4d32364987eed
bangddong <gusehdqkd@naver.com>
2024-02-05 18:54:21 +0900

    router loader 실습

---
+++ spa-2/frontend/src/pages/Events.js
@@ -0,0 +1,11 @@
+import { useLoaderData } from "react-router-dom";
+
+import EventsList from "../components/EventsList";
+
+function EventsPage() {
+  const fetchedEvents = useLoaderData();
+
+  return <EventsList events={fetchedEvents} />;
+}
+
+export default EventsPage;
