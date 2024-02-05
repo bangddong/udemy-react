44bf5301db406a974de82c29229f2969805b5688
bangddong <gusehdqkd@naver.com>
2024-02-05 17:12:04 +0900

    과제 2번 기본 라우터 생성

--- spa-2/frontend/src/App.js
+++ spa-2/frontend/src/App.js
@@ -20,8 +20,28 @@
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
+import { RouterProvider, createBrowerRouter } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import EventsPage from "./pages/EventsPage";
+import EventDetailPage from "./pages/EventDetailPage";
+import NewEventPage from "./pages/NewEventPage";
+import EditEventPage from "./pages/EditEventPage";
+
+const router = createBrowerRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    children: [
+      { path: "/events", element: <EventsPage /> },
+      { path: "/events/:eventId", element: <EventDetailPage /> },
+      { path: "/events/new", element: <NewEventPage /> },
+      { path: "/events/:eventId/edit", element: <EditEventPage /> },
+    ],
+  },
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
