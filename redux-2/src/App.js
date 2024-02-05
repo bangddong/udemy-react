b1bb5ba95c80ecb9fcdecfc225236476614d5bab
bangddong <gusehdqkd@naver.com>
2024-02-05 14:20:22 +0900

    refactor - app 경량화  - app.js 내 API 호출 로직 cart-slice로 이관  - 복잡한 작업은 redux에 넘겨 컴포넌트를 경량화 한다

--- redux-2/src/App.js
+++ redux-2/src/App.js
@@ -4,8 +4,8 @@ import { useEffect } from "react";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { uiActions } from "./stroe/ui-slice";
 import Notification from "./components/UI/Notification";
+import { sendCartData } from "./stroe/cart-slice";
 
 let isInitial = true;
 
@@ -16,48 +16,12 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending...",
-          message: "Sending cart data!",
-        })
-      );
-      console.log(cart);
-
-      const response = await fetch("http://localhost:8080/api/cart", {
-        method: "PUT",
-        body: JSON.stringify(cart.items),
-      });
-
-      if (!response.ok) {
-        throw new Error("Sending cart data failed.");
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "SUccess!",
-          message: "Send cart data successfully!",
-        })
-      );
-    };
-
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error!",
-          message: "Sending cart data failed!",
-        })
-      );
-    });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
