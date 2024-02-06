8e95f58a35fc5576fb0fc9543be0e2e16551db22
bangddong <gusehdqkd@naver.com>
2024-02-06 14:44:06 +0900

    refactor - use react-router-dom json  - 기존에 new Response로 JSON 직렬화 및 역직렬화 해서 사용한 부분 수정  - 라이브러리에서 제공하는 json 객체를 이용 코드 단순화

--- spa-2/frontend/src/pages/EventDetail.js
+++ spa-2/frontend/src/pages/EventDetail.js
@@ -1,19 +1,26 @@
-import { Link, useParams } from "react-router-dom";
+import { json, useLoaderData } from "react-router-dom";
+
+import EventItem from "./../components/EventItem";
 
 function EventDetailPage() {
-  const params = useParams();
+  const data = useLoaderData();
 
   return (
     <>
-      <h1>EventDetailPage</h1>
-      <p>{params.eventId}</p>
-      <p>
-        <Link to=".." relative="path">
-          Back
-        </Link>
-      </p>
+      <EventItem event={data.event} />
     </>
   );
 }
 
 export default EventDetailPage;
+
+export async function loader({ request, params }) {
+  const id = params.eventId;
+  const response = await fetch("http://localhost:8080/events/" + id);
+
+  if (!response.ok) {
+    throw json({ message: "Could not fetch event." }, { status: 500 });
+  } else {
+    return response;
+  }
+}
