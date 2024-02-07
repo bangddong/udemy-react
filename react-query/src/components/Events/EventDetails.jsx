1539696be9055b4ad5a51c704c6fa3140df806a9
bangddong <gusehdqkd@naver.com>
2024-02-07 17:02:47 +0900

    close #1 event detail 데이터 출력

--- react-query/src/components/Events/EventDetails.jsx
+++ react-query/src/components/Events/EventDetails.jsx
@@ -1,8 +1,36 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useParams } from "react-router-dom";
 
-import Header from '../Header.jsx';
+import Header from "../Header.jsx";
+import { useQuery } from "@tanstack/react-query";
+import { fetchEvent } from "../../util/http.js";
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EventDetails() {
+  const params = useParams();
+  const id = params.id;
+
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["event"],
+    queryFn: ({ signal }) => fetchEvent({ signal, id }),
+    staleTime: 5000,
+  });
+
+  let content;
+
+  if (isPending) {
+    content = <LoadingIndicator />;
+  }
+
+  if (isError) {
+    content = (
+      <ErrorBlock
+        title="An error occurred"
+        message={error.info?.message || "Failed to fetch event."}
+      />
+    );
+  }
+
   return (
     <>
       <Outlet />
@@ -11,25 +39,31 @@ export default function EventDetails() {
           View all Events
         </Link>
       </Header>
-      <article id="event-details">
-        <header>
-          <h1>EVENT TITLE</h1>
-          <nav>
-            <button>Delete</button>
-            <Link to="edit">Edit</Link>
-          </nav>
-        </header>
-        <div id="event-details-content">
-          <img src="" alt="" />
-          <div id="event-details-info">
-            <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
+
+      {!data && content}
+      {data && (
+        <article id="event-details">
+          <header>
+            <h1>{data.title}</h1>
+            <nav>
+              <button>Delete</button>
+              <Link to="edit">Edit</Link>
+            </nav>
+          </header>
+          <div id="event-details-content">
+            <img src={`http://localhost:3000/${data.image}`} alt={data.title} />
+            <div id="event-details-info">
+              <div>
+                <p id="event-details-location">{data.location}</p>
+                <time
+                  dateTime={`${data.date}T${data.time}`}
+                >{`${data.date} @ ${data.time}`}</time>
+              </div>
+              <p id="event-details-description">{data.description}</p>
             </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
           </div>
-        </div>
-      </article>
+        </article>
+      )}
     </>
   );
 }
