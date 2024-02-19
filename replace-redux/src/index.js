77db918eec7a5ac370b7263be702391a78070b36
bangddong <gusehdqkd@naver.com>
2024-02-19 15:30:39 +0900

    refactor : use custom hook  - 별도의 useStore 커스텀 훅을 구현하여 기존의 context를 대체

--- replace-redux/src/index.js
+++ replace-redux/src/index.js
@@ -4,14 +4,13 @@ import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
-import ProductsProvider from "./context/products-context";
+import configureProductsStore from "./hooks-stroe/products-store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+configureProductsStore();
 root.render(
-  <ProductsProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ProductsProvider>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
